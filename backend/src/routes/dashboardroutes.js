5eea7adcfa04505693ac91772da738bad7f36b7f
Sonu Sharma <sonusharma122416@gmail.com>
2026-07-27 21:09:13 +0530

    Fix import case for dashboardController

--- backend/src/routes/dashboardroutes.js
+++ backend/src/routes/dashboardroutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getDashboardStats } = require("../controllers/dashboardController");
+const { getDashboardStats } = require("../controllers/dashboardcontroller");
 const loginMiddleware = require("../middleware/loginmiddleware");
 
 router.get("/", loginMiddleware, getDashboardStats);
