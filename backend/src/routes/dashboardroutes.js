2ab7e768d9a10a2dc70851d2e4664274783c6a06
Sonu Sharma <sonusharma122416@gmail.com>
2026-07-27 21:01:35 +0530

    Fix import case for login middleware

--- backend/src/routes/dashboardroutes.js
+++ backend/src/routes/dashboardroutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { getDashboardStats } = require("../controllers/dashboardController");
-const loginMiddleware = require("../middleware/loginMiddleware");
+const loginMiddleware = require("../middleware/loginmiddleware");
 
 router.get("/", loginMiddleware, getDashboardStats);
 
