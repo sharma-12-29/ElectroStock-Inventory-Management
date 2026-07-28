0f0305e3f3392fa90f6db98b367078d7b4b3277c
Sonu Sharma <sonusharma122416@gmail.com>
2026-07-28 11:53:27 +0530

    Change API base URL to production server

--- frontend/vite-project/src/api/axios.js
+++ frontend/vite-project/src/api/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
-
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://electrostock-inventory-management.onrender.com/api",
 });
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
