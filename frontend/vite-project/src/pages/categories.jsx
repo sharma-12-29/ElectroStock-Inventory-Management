1560b1eb9e6a573f208880d7de8fc565019bfea1
Sonu Sharma <sonusharma122416@gmail.com>
2026-07-28 12:42:27 +0530

    Update API URLs to use production endpoint

--- frontend/vite-project/src/pages/categories.jsx
+++ frontend/vite-project/src/pages/categories.jsx
@@ -48,7 +48,7 @@ const addCategory = async () => {
 
     if (editId) {
       await axios.put(
-        `http://localhost:3000/api/categories/${editId}`,
+         `https://electrostock-inventory-management.onrender.com/api/categories/${editId}`,
         formData,
         {
           headers: {
@@ -58,7 +58,7 @@ const addCategory = async () => {
       );
     } else {
       await axios.post(
-        "http://localhost:3000/api/categories/add",
+       "https://electrostock-inventory-management.onrender.com/api/categories/add",
         formData,
         {
           headers: {
@@ -109,7 +109,7 @@ const deleteCategory = async (id) => {
     const token = localStorage.getItem("token");
 
     await axios.delete(
-      `http://localhost:3000/api/categories/${id}`,
+       `https://electrostock-inventory-management.onrender.com/api/categories/${id}`,
       {
         headers: {
           Authorization: `Bearer ${token}`,
