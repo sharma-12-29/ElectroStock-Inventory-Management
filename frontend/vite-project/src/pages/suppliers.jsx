393917e02d279670bd45688384f9bd4cfcf03426
sharma-12-29 <sonusharma122416@gamil.com>
2026-07-28 12:28:14 +0530

    fix category supplier api url

--- frontend/vite-project/src/pages/suppliers.jsx
+++ frontend/vite-project/src/pages/suppliers.jsx
@@ -54,11 +54,7 @@ const [formData, setFormData] = useState({
     const token = localStorage.getItem("token");
 
     const res = await axios.get(
-<<<<<<< HEAD
-      "https://electrostock-inventory-management.onrender.com/api/suppliers/all",
-=======
-         "https://electrostock-inventory-management.onrender.com/api/suppliers/all",
->>>>>>> 13a7f8e4ad45aa91fc9598c2f63eb1e419c19269
+  "https://electrostock-inventory-management.onrender.com/api/suppliers/all",
       {
         headers:{
           Authorization:`Bearer ${token}`
@@ -86,11 +82,8 @@ const addSupplier = async()=>{
   if(editId){
 
     await axios.put(
-<<<<<<< HEAD
   `https://electrostock-inventory-management.onrender.com/api/suppliers/${editId}`,
-=======
- `https://electrostock-inventory-management.onrender.com/api/suppliers/${editId}`,
->>>>>>> 13a7f8e4ad45aa91fc9598c2f63eb1e419c19269
+
       formData,
       {
         headers:{
